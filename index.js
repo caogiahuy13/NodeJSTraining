3e5a73073b59e473778909079d60fa3f529336df
caogiahuy13 <caogiahuy13@gmail.com>
2020-05-10 22:14:24 +0900

    4509496: Environment Variables

--- index.js
+++ index.js
@@ -10,4 +10,5 @@ app.get("/customers", (req,res) => {
     res.send([1,2,3]);
 })
 
-app.listen(3000, () => console.log("Listening on port 3000"))
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
