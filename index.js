5b538a2feb509e3afa92d096bc68dc757aaae4ab
caogiahuy13 <caogiahuy13@gmail.com>
2020-05-10 22:57:31 +0900

    4509497: POST request

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 const courses = [
     { id: 1, name: "course 1"},
     { id: 2, name: "course 2"},
@@ -22,6 +24,15 @@ app.get("/api/courses/:id", (req, res) => {
     } else {
         res.status(404).send("Course not found");
     }
+})
+
+app.post("/api/courses", (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(course);
+    res.send(course);
 })
 
 const port = process.env.PORT || 3000;
