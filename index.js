756e3aed63637914fb7377eae959b73a8330a526
caogiahuy13 <caogiahuy13@gmail.com>
2020-05-10 22:37:52 +0900

    4509499: handle request

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
+const courses = [
+    { id: 1, name: "course 1"},
+    { id: 2, name: "course 2"},
+    { id: 3, name: "course 3"}
+]
 app.get("/", (req,res) => {
     res.send("Hello World");
 });
@@ -10,8 +15,14 @@ app.get("/customers", (req,res) => {
     res.send([1,2,3]);
 })
 
-app.get("api/posts/:year/:month", (req, res) => {
-    res.send(req.param);
+app.get("/api/courses/:id", (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (course) {
+        res.send(course);
+    } else {
+        res.status(404).send("Course not found");
+    }
 })
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
