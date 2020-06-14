5705cc1d754eb628b6bee96952dbffdc0c4526c9
caogiahuy13 <caogiahuy13@gmail.com>
2020-06-14 20:41:27 +0900

    13901632: Sending Response

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+  res.setHeader("Content-Type", "text/html");
+  res.write("<h1>Hello World</h1>");
+  res.end;
 });
 
 server.listen(3000);
