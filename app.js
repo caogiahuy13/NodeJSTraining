b4aa1d612da4641e6d9f422439701c7a01009951
caogiahuy13 <caogiahuy13@gmail.com>
2020-06-14 21:18:05 +0900

    13901630: Redirecting Requests

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
   if (url === "/") {
     res.write("<h1>Hello World</h1>");
+    res.write('<form action="message" method="POST">input</form>');
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "TEST");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
   }
 
   res.setHeader("Content-Type", "text/html");
