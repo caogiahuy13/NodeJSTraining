647afad94247963b98a7d5b1eaf74184122d2744
caogiahuy13 <caogiahuy13@gmail.com>
2020-06-14 21:05:49 +0900

    13901628: Routing Request

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === "/") {
+    res.write("<h1>Hello World</h1>");
+  }
+
   res.setHeader("Content-Type", "text/html");
   res.write("<h1>Hello World</h1>");
   res.end;
