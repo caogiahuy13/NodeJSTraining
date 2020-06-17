9ab611dcea05f3da5028cf4c08ecd0f5634cd0fd
caogiahuy13 <caogiahuy13@gmail.com>
2020-06-17 22:40:59 +0900

    13901711: Handling Different Routes

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-const http = require("http");
-
 const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("first middleware");
-  next();
+app.use("/add-product", (req, res, next) => {
+  console.log("Another middleware");
+  res.send("<h1>Another Middleware</h1>");
 });
+
 app.use((req, res, next) => {
   console.log("Another middleware");
   res.send("<h1>Hello World</h1>");
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
