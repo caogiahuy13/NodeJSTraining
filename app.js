e674190ad9973fdb0c8bd056e37cfa27542f6111
caogiahuy13 <caogiahuy13@gmail.com>
2020-06-17 23:23:39 +0900

    13901722: Parsing Incoming Requests

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use("/add-product", (req, res, next) => {
-  console.log("Another middleware");
-  res.send("<h1>Another Middleware</h1>");
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add product</button></form>"
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use((req, res, next) => {
-  console.log("Another middleware");
   res.send("<h1>Hello World</h1>");
 });
 
