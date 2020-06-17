ae2e7997a2d14129745ccbee18d6833a9f11cc2b
caogiahuy13 <caogiahuy13@gmail.com>
2020-06-17 23:34:11 +0900

    13901724: Limiting Middleware Execution to Post requests

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
+app.post("/add-product", (req, res, next) => {
   res.send(
     "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add product</button></form>"
   );
